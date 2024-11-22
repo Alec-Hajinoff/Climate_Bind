3cb1bc728919b931dade0de382c51fbf689e9ad1
Alec Hajinoff <hajinoff@gmail.com>
2024-11-22 17:57:53 +0000

    22.11.24, 17.57

--- script.js
+++ script.js
@@ -10,41 +10,10 @@ const appendAlert = (message, type) => {
 
     alertPlaceholder.append(wrapper)
 }
-console.log("zero");
-const alertTrigger = document.getElementById('liveAlertBtn');
-console.log("zeroOne");
-const inputs = document.querySelectorAll('input');
-console.log("zeroTwo");
-console.log(inputs); 
-inputs.forEach(input => {
-    if (input.value !== '') {
-        alertTrigger.addEventListener('click', () => {
-            appendAlert('Thank you, we received your message and will be in touch soon!', 'success')
-    })
-} else {
-    console.log("three");
-    alertTrigger.addEventListener('click', () => {
-        appendAlert('We need more information, please!', 'success')
-    })
-    console.log("four");
-}
-})
 
-/*
 const alertTrigger = document.getElementById('liveAlertBtn');
-const inputs = document.querySelectorAll('input.form-control');
-inputs.forEach(input => {
-    if (input.value !== '') {
-        alertTrigger.addEventListener('click', () => {
-            appendAlert('Thank you, we received your message and will be in touch soon!', 'success')
-    })
-}
-});
-*/
-
-/*
-if ((alertTrigger) && (inputs !== false)) {
+if (alertTrigger) {
     alertTrigger.addEventListener('click', () => {
         appendAlert('Thank you, we received your message and will be in touch soon!', 'success')
     })
-} */
+} 
