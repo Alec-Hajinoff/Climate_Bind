1ee0e44d8f4dda56aeecdb52f267f786a3bbd8cf
Alec Hajinoff <hajinoff@gmail.com>
2024-11-23 07:07:23 +0000

    23.11.24, 7.07

--- script.js
+++ script.js
@@ -11,9 +11,39 @@ const appendAlert = (message, type) => {
     alertPlaceholder.append(wrapper)
 }
 
+const form = document.getElementById('myForm');
+const nameInput = document.getElementById('yourName');
+const emailInput = document.getElementById('yourEmail');
+const yourNumber = document.getElementById('yourNumber');
+const yourComments = document.getElementById('yourComments');
+
+function validateForm() {
+
+    if (nameInput.value === '') {
+        return false;
+    }
+
+    if (emailInput.value === '') {
+        return false;
+    }
+
+    if (yourNumber.value === '') {
+        return false;
+    }
+
+    if (yourComments.value === '') {
+        return false;
+    }
+
+    return true;
+}
+
 const alertTrigger = document.getElementById('liveAlertBtn');
-if (alertTrigger) {
-    alertTrigger.addEventListener('click', () => {
+
+alertTrigger.addEventListener('click', () => {
+    if ((alertTrigger) && (validateForm())) {
         appendAlert('Thank you, we received your message and will be in touch soon!', 'success')
-    })
-} 
+
+    }
+});
+
