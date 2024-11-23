4d5ed4fbe59fe38170c736a157d397b410b3f0da
Alec Hajinoff <hajinoff@gmail.com>
2024-11-23 18:03:47 +0000

    23.11.24, 18.03

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var wrapper = null;
 const appendAlert = (message, type) => {
-    const wrapper = document.createElement('div')
+    if (!wrapper) {
+        wrapper = document.createElement('div');
+        alertPlaceholder.append(wrapper)
+    }
     wrapper.innerHTML = [
         `<div class="alert alert-${type} alert-dismissible" role="alert">`,
         `   <div>${message}</div>`,
         '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
         '</div>'
     ].join('')
-
-    alertPlaceholder.append(wrapper)
 }
+
 
 const nameInput = document.getElementById('yourName');
 const emailInput = document.getElementById('yourEmail');
@@ -17,35 +20,36 @@ const yourNumber = document.getElementById('yourNumber');
 const yourComments = document.getElementById('yourComments');
 
 function validateForm() {
-    console.log("1");
+
 
     if (nameInput.value === '') {
         return false;
     }
-    console.log("2");
+
     if (emailInput.value === '') {
         return false;
     }
-    console.log("3");
+
     if (yourNumber.value === '') {
         return false;
     }
-    console.log("4");
+
     if (yourComments.value === '') {
         return false;
     }
 
     return true;
-    console.log("5");
+
 }
 
 const alertTrigger = document.getElementById('liveAlertBtn');
 
-alertTrigger.addEventListener('click', () => {
-    console.log("6");
+alertTrigger.addEventListener('click', (event) => {
+
     if ((alertTrigger) && (validateForm())) {
-        console.log("7");
-        setTimeout((appendAlert('Thank you, we received your message and we will be in touch soon!', 'success'), 5000));
+        event.preventDefault();
+
+        appendAlert('Thank you, we received your message and we will be in touch soon!', 'success');
     }
 });
 
