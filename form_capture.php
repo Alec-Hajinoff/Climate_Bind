1b4c3f87c70f365ea4011baf82486d7a1de9b101
Alec Hajinoff <hajinoff@gmail.com>
2024-11-25 17:59:38 +0000

    25.11.24, 17.59

--- form_capture.php
+++ form_capture.php
@@ -1,5 +1,9 @@
 <?php
+
 $to = 'hajinoff@gmail.com';
 $subject = 'Test Email';
-$message = $_POST['fname'];
-mail($to, $subject, $message); 
+$message = $_POST['fname']; 
+mail($to, $subject, $message);  
+echo "<p>Copyright &copy; 1999-" . date("Y") . " W3Schools.com</p>";
+
+?>
